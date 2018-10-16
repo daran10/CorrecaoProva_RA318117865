eaa72faaabe0dcc451dd39191ef3295c2779e4f0
daran10 <36827107+daran10@users.noreply.github.com>
2018-10-16 20:44:07 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/IMC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+        public double Classificar(double imc)
+
+        {
+            return 0.0;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int Maior(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+
+            }
+            else
+            {
+                return b;
+            }
+        }
+            static bool Par( int num)
+            {
+                int resto = num % 2;
+                if (resto == 0)
+                    return true;
+                else
+                    return false;
+            }
+
+
+            static void Main(string[] args)
+        {
+
+        }
+    }
+}
