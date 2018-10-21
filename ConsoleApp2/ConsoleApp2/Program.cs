a700cfc5c8c2a54796c8af398ecefece8bf0d3a4
daran10 <36827107+daran10@users.noreply.github.com>
2018-10-21 20:24:37 -0300

    Add files via upload

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,35 +1,61 @@
 ﻿using System;
 
-
-namespace ConsoleApp2
+namespace CorreçãoQuestao2
 {
-    class Program
+    class MainClass
     {
-        static int Maior(int a, int b)
-        {
-            if (a > b)
-            {
-                return a;
-
-            }
-            else
-            {
-                return b;
-            }
-        }
-            static bool Par( int num)
-            {
-                int resto = num % 2;
-                if (resto == 0)
-                    return true;
-                else
-                    return false;
-            }
-
-
-            static void Main(string[] args)
+        public static void Main(string[] args)
         {
 
+            double peso = 0;
+            double altura = 0;
+            double ResultadoIMC = 0;
+
+
+            Console.WriteLine("Digite o seu Peso : ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua Altura : ");
+            altura = double.Parse(Console.ReadLine());
+
+            IMC m = new IMC();
+            ResultadoIMC = m.Calcular(peso, altura);
+
+
+            Console.WriteLine("O seu IMC é : " + ResultadoIMC);
+
+            Console.WriteLine("Você está com a classificaçào " + m.Classificar(ResultadoIMC));
+
+            if (ResultadoIMC < 16)
+            {
+                Console.WriteLine("Magreza Grave");
+                Console.ReadKey();
+            }
+            if (ResultadoIMC <= 17)
+            {
+                Console.WriteLine("Magreza Moderada");
+            }
+            if (ResultadoIMC >= 16)
+            {
+                Console.WriteLine("Magreza Moderada");
+            }
+
+            if (ResultadoIMC >= 17)
+            {
+                Console.WriteLine("Magreza Leve");
+            }
+            if (ResultadoIMC <= 18.5)
+            {
+                Console.WriteLine("Magreza Leve");
+            }
+            if (ResultadoIMC <= 25)
+            {
+                Console.WriteLine("Saudável");
+            }
+            if (ResultadoIMC <= 30)
+            {
+                Console.WriteLine("Sobrepeso");
+            }
         }
     }
 }
