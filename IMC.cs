5fa09be71509232398b64e1d0e3b1c66d33e23ee
daran10 <36827107+daran10@users.noreply.github.com>
2018-10-21 21:53:54 -0300

    Add files via upload

---
+++ IMC.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace CorreçãoQuestao2
+{
+    class IMC
+    {
+        private double peso;
+        private double altura;
+
+        public IMC()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+
+        }
+
+        public IMC(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double Calcular(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+
+
+        public string Classificar(double m)
+        {
+            if (m < 16)
+            {
+                return "Magreza Grave";
+
+            }
+            else if (m > 16 && m < 17)
+            {
+                return "Magreza Moderada";
+            }
+            else if (m > 17 && m < 18.5)
+            {
+                return "Magreza Leve";
+            }
+            else if (m > 18.5 && m < 25)
+            {
+                return "Saudável";
+            }
+            else if (m > 25 && m < 30)
+            {
+                return "Sobrepeso";
+            }
+            else if (m > 30 && m < 35)
+            {
+                return "Obesidade Grau I";
+            }
+            else if (m > 35 && m < 40)
+            {
+                return "Obesidade Grau II (SEVERA)";
+            }
+            else if (m > 40)
+            {
+                return "Obesidade Grau morbida";
+            }
+
+            return "";
+        }
+    }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CorreçãoQuestao2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            double peso = 0;
+            double altura = 0;
+            double ResultadoIMC = 0;
+
+
+            Console.WriteLine("Digite o seu Peso : ");
+            peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua Altura : ");
+            altura = double.Parse(Console.ReadLine());
+
+            IMC m = new IMC();
+            ResultadoIMC = m.Calcular(peso, altura);
+
+
+            Console.WriteLine("O seu IMC é : " + ResultadoIMC);
+
+            Console.WriteLine("Você está com a classificaçào " + m.Classificar(ResultadoIMC));
+
+          
+        }
+    }
+}
